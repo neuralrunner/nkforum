5a83e7f89ef89cc0875a756258e2af3dc0dc74d1
neuralrunner <brunohcagnin@gmail.com>
2020-09-11 12:11:46 -0300

    Project Creation

---
+++ src/main/java/codes/neuralkatana/nkforum/NkforumApplication.java
@@ -0,0 +1,13 @@
+package codes.neuralkatana.nkforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NkforumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NkforumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/codes/neuralkatana/nkforum/NkforumApplicationTests.java
@@ -0,0 +1,13 @@
+package codes.neuralkatana.nkforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NkforumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
