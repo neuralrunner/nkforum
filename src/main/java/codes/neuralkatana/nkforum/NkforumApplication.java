920e25b01da5f461fe51b81c6cf91380db24813e
neuralrunner <brunohcagnin@gmail.com>
2020-11-03 10:47:12 -0300

    Changes to make the application to compile as a War package.

--- src/main/java/codes/neuralkatana/nkforum/NkforumApplication.java
+++ src/main/java/codes/neuralkatana/nkforum/NkforumApplication.java
@@ -2,20 +2,25 @@ package codes.neuralkatana.nkforum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
 @EnableCaching
 @EnableSwagger2
-public class NkforumApplication {
+public class NkforumApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(NkforumApplication.class, args);
         //System.out.println("PASSWORD ENCODE:" + new BCryptPasswordEncoder().encode("123456"));
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(NkforumApplication.class);
+    }
 }
